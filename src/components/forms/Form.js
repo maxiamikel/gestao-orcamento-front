290df009e849e5267566d5f4ec1980df6ef215db
maxiamikel <maxloversist@gmail.com>
2022-07-05 20:35:30 -0300

    Conexao com Json-server e GET nas categirias

--- src/components/forms/Form.js
+++ src/components/forms/Form.js
@@ -1,9 +1,30 @@
+import { useState, useEffect } from 'react'
 import React from 'react'
+
 import './Form.css'
+
 import Input from './Input'
 import Select from './Secetc'
 import SubmitBtn from './SubmitBtn'
+
 function Form({btnLabel}){
+    const [categorias, setCategorias] = useState([]);
+
+    useEffect(() =>{
+            fetch("http://localhost:5000/categorias",{
+            method:"GET",
+            headers:{
+                'Content-Type':'application/json'
+            }
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            setCategorias(data)
+        })
+        .catch((err) => console.log(err))
+
+    },[])
+
     return(
         <div>
             <form className="form">
@@ -25,7 +46,7 @@ function Form({btnLabel}){
                     text="Project owner name"
                     placeholder="Fill the project owner name"
                 />
-                <Select name="category_id" text="Select the project category"/>
+                <Select name="category_id" text="Select the project category" options={categorias}/>
                 <SubmitBtn text={btnLabel} />
             </form>
         </div>
--- src/components/forms/Secetc.js
+++ src/components/forms/Secetc.js
@@ -1,10 +1,15 @@
 import './Select.css'
-function Select({text,name,option,handleOneChange,value}){
+function Select({text,name,options,handleOneChange,value}){
     return(
         <div className="control_form ">
             <label htmlFor={name}>{text}:</label>
             <select name={name} id={name}>
                 <option>Select an option</option>
+                {options.map((option) => (
+                    <option value={option.id} key={option.id}>
+                        {option.nome}
+                    </option>
+                ))}
             </select>
         </div>
     )
